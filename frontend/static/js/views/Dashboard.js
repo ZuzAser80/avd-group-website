6abbeb90df5c9d4661c1d16fd5a6507680bffc71
zaeeeeeb <alisa.safina.01@mail.ru>
2026-07-15 17:47:50 +0500

    ну чето еще

---
+++ frontend/static/js/views/Dashboard.js
@@ -0,0 +1,47 @@
+const Dashboard = {
+    template: `
+        <div class="dashboard">
+            <div class="dashboard-header">
+                <img src="/static/images/logo.png" alt="АВД ГРУПП" class="logo">
+                <h1>АВД Групп</h1>
+                <button class="button btn-logout" @click="handleLogout">Выйти</button>
+            </div>
+            <div class="dashboard-content">
+                <div class="card">
+                    <h2>Профиль</h2>
+                    <div v-if="user">
+                        <div class="info-row">
+                            <span class="label">ID</span>
+                            <span class="value">{{ user.sub }}</span>
+                        </div>
+                        <div class="info-row">
+                            <span class="label">Токен истекает</span>
+                            <span class="value">{{ exp }}</span>
+                        </div>
+                    </div>
+                    <p v-else style="color:#666">Нет данных</p>
+                </div>
+            </div>
+        </div>
+    `,
+    computed: {
+        user() {
+            return API.parseToken();
+        },
+        exp() {
+            if (!this.user || !this.user.exp) return '';
+            return new Date(this.user.exp * 1000).toLocaleString('ru-RU');
+        },
+    },
+    created() {
+        if (!API.isLoggedIn()) {
+            this.$router.push('/login');
+        }
+    },
+    methods: {
+        handleLogout() {
+            API.logout();
+            this.$router.push('/login');
+        },
+    },
+};
